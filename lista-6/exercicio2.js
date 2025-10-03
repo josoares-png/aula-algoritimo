ce50b77da488cf5a35a5b53259a57b7eb0f7fb96
Jovanna Soares <joo.soaress32@gmail.com>
2025-10-03 14:31:43 -0300

    exercicio2.js

---
+++ lista-6/exercicio2.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+let n = parseInt(prompt("Digite um número: "));
+
+for (let i = n + 1; i < 20; i++) {
+  console.log(i);
+}
+for (let i = n - 1; i > 20; i--) {
+  console.log(i);
+
