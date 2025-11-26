46423ca77af6fdedbe1f1299b22342e83e8323aa
Jovanna Soares <joo.soaress32@gmail.com>
2025-11-26 13:19:30 -0300

    Update and rename exercicio2.js to ex1.js

---
+++ lista-6.assincrona/ex1.js
@@ -0,0 +1,7 @@
+const input = require('readline-sync');
+
+console.log("Mostre todos os números de 1 até 20")
+for(let numero =1; numero <20; numero ++){
+    console.log("o numero = ", numero);
+}
+
